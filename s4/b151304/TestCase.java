e2780804b790ddd73f5f4a89686ad722fec50246
Daichi Hadama <d151304@edu.tut.ac.jp>
2017-12-19 16:25:21 +0900

    Frequencer.javaにおけるスペースが0文字とターゲットがHoのテストケースを追加

--- s4/b151304/TestCase.java
+++ s4/b151304/TestCase.java
@@ -95,28 +95,28 @@ public class TestCase {
   }
 
 
-	// try {
-	//     InformationEstimatorInterface myObject;
-	//     double value;
-	//     System.out.println("checking s4.b151304.InformationEstimator");
-	//     myObject = new s4.b151304.InformationEstimator();
-	//     myObject.setSpace("3210321001230123".getBytes());
-	//     myObject.setTarget("0".getBytes());
-	//     value = myObject.estimation();
-	//     System.out.println(">0 "+value);
-	//     myObject.setTarget("01".getBytes());
-	//     value = myObject.estimation();
-	//     System.out.println(">01 "+value);
-	//     myObject.setTarget("0123".getBytes());
-	//     value = myObject.estimation();
-	//     System.out.println(">0123 "+value);
-	//     myObject.setTarget("00".getBytes());
-	//     value = myObject.estimation();
-	//     System.out.println(">00 "+value);
-	// }
-	// catch(Exception e) {
-	//     System.out.println("Exception occurred: STOP");
-	// }
+	try {
+	    InformationEstimatorInterface myObject;
+	    double value;
+	    System.out.println("checking s4.b151304.InformationEstimator");
+	    myObject = new s4.b151304.InformationEstimator();
+	    myObject.setSpace("3210321001230123".getBytes());
+	    myObject.setTarget("0".getBytes());
+	    value = myObject.estimation();
+	    System.out.println(">0 "+value);
+	    myObject.setTarget("01".getBytes());
+	    value = myObject.estimation();
+	    System.out.println(">01 "+value);
+	    myObject.setTarget("0123".getBytes());
+	    value = myObject.estimation();
+	    System.out.println(">0123 "+value);
+	    myObject.setTarget("00".getBytes());
+	    value = myObject.estimation();
+	    System.out.println(">00 "+value);
+	}
+	catch(Exception e) {
+	    System.out.println("Exception occurred: STOP");
+	}
 
     }
 }
